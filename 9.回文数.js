209ba288a5c915cf2e83d031e8f2f383f3f879c9
linguanghui <1476465526@qq.com>
2022-11-30 11:18:23 +0800

    9.回文数

---
+++ 9.回文数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=javascript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x > 0 ? x : x * -1;
+
+    const str = `${x}`;
+
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+// @lc code=end
+
